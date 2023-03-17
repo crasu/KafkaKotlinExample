4176bed9be9c8af79eb7d4510ec6bce696d8f3a6
Christian Krämer <spam@crasu.de>
2023-03-17 10:39:36 +0100

    Added kafka example

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,56 @@
+import org.apache.kafka.clients.consumer.ConsumerRecords
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.ProducerRecord
+import java.time.Duration
+import java.util.*
+
+const val TOPIC = "my-topic"
+
+fun testProducer() {
+    val props = Properties()
+    props["bootstrap.servers"] = "localhost:9092"
+    props["acks"] = "all"
+    props["key.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
+    props["value.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
+
+    val producer: Producer<String, String> = KafkaProducer(props)
+    for (i in 0..99) producer.send(
+        ProducerRecord(
+            TOPIC,
+            Integer.toString(i), Integer.toString(i)
+        )
+    )
+    producer.close()
+}
+
+fun testConsumer() {
+    val props = Properties()
+    props.setProperty("bootstrap.servers", "localhost:9092")
+    props.setProperty("group.id", "test")
+    props.setProperty("enable.auto.commit", "true")
+    props.setProperty("auto.commit.interval.ms", "1000")
+    props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+
+    val consumer: KafkaConsumer<String, String> = KafkaConsumer(props)
+    consumer.subscribe(listOf(TOPIC))
+    while (true) {
+        val records: ConsumerRecords<String?, String?> = consumer.poll(Duration.ofMillis(5000))
+        if(!records.isEmpty) {
+            for (record in records) println(
+                "offset = ${record.offset()}, key = ${record.key()}, value = ${record.value()}"
+            )
+        } else {
+            println("no message(s)")
+        }
+    }
+}
 fun main(args: Array<String>) {
     println("Hello World!")
+    testProducer();
+    testConsumer();
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 }

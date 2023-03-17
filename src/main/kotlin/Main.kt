4ea40c4d6c4dcefa6b6cfb0303e24145d2373d8f
Christian Krämer <spam@crasu.de>
2023-03-17 11:11:27 +0100

    Added Consumer constants instead of keys

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.ConsumerRecords
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.clients.producer.KafkaProducer
@@ -27,12 +28,12 @@ fun testProducer() {
 
 fun testConsumer() {
     val props = Properties()
-    props.setProperty("bootstrap.servers", "localhost:9092")
-    props.setProperty("group.id", "test")
-    props.setProperty("enable.auto.commit", "true")
-    props.setProperty("auto.commit.interval.ms", "1000")
-    props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-    props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "test")
+    props.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true")
+    props.setProperty(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000")
+    props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+    props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
 
     val consumer: KafkaConsumer<String, String> = KafkaConsumer(props)
     consumer.subscribe(listOf(TOPIC))
@@ -48,7 +49,6 @@ fun testConsumer() {
     }
 }
 fun main(args: Array<String>) {
-    println("Hello World!")
     testProducer();
     testConsumer();
 
